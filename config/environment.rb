07cf5b9bcc07dbd733cecc908b30ab2ddb2d05f9
Nate Wise <nathanwise@gmail.com>
2010-08-07 12:44:50 -0400

    Updating environment so that it uses the propper gem name.

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'will_paginate'
   config.gem "authlogic"
   config.gem "searchlogic"
   # Settings in config/environments/* take precedence over those specified here.
