569961cf7001f3d5cceb160f18ad94f29d10cd37
Nathan Wise <nathanwise@gmail.com>
2016-04-19 19:56:15 -0400

    Fixing email.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ActionMailer::Base
 
   def contact_us_email( contents )
     @contents = contents
-    mail(to: Ohiorealtygroup::Application.config.contact_recipient, bcc: 'nathan@wise-idea.com', subject: 'Contact Us Form', from: @contents.email_address, headers: {}) do |format|
+    mail(to: Ohiorealtygroup::Application.config.contact_recipient, bcc: 'nathan@wise-ideas.com', subject: 'Contact Us Form', from: @contents.email_address, headers: {}) do |format|
       format.text { render :contact_us_email }
     end
   end
